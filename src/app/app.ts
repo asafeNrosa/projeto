201bd5361a31a5b32856c1dbd2bc34cdc17d1ade
Asafe Nogueira Rosa <asaferosa1@gmail.com>
2026-01-16 18:15:26 -0300

    Lista funcional

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ListaCompras } from './lista-compras/lista-compras';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  standalone: true,
+  imports: [ListaCompras],
+    templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('projeto');
+  title = 'projeto';
 }
---
+++ src/app/lista-compras/lista-compras.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+
+@Component({
+  selector: 'app-lista-compras',
+  standalone: true,
+  imports: [ FormsModule ],
+  templateUrl: './lista-compras.html',
+  styleUrl: './lista-compras.scss'
+})
+export class ListaCompras {
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    if (this.item.trim() === '') return;
+
+    const novoItem = new ItemLista();
+    novoItem.nome = this.item;
+    novoItem.id = this.lista.length + 1;
+
+    this.lista.push(novoItem);
+    this.item = ''; 
+  }
+}
